50777d501f0b070744bc431b2dc95b98bd05ce2e
antoine <antoine.weisse@epitech.eu>
2024-12-05 21:55:16 +0100

    [add] page load

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <h1>Hello world</h1>
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
 
 <script>
 
 export default {
-  name: 'HomePage',
+  name: 'App',
 }
+
 </script>
 
+
+
 <style>
+
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ front/src/pages/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>
+            Home Page
+        </h1>
+        <HelloComponent/>
+    </div>
+</template>
+
+<script>
+
+import HelloComponent from '@/component/HelloComponent.vue';
+
+
+export default {
+    name: 'HomePage',
+    components: {
+        HelloComponent
+    }
+}
+
+</script>
