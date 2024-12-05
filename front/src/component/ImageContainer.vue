bee6745f9ed7bcd5f489db7640a2de0779913eaf
antoine <antoine.weisse@epitech.eu>
2024-12-05 22:10:58 +0100

    [add] img

---
+++ front/src/component/ImageContainer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <img :src="require('@/assets/coeur.jpg')" alt="Vue logo" />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'ImageContainer',
+}
+
+</script>
--- front/src/pages/HomePage.vue
+++ front/src/pages/HomePage.vue
@@ -4,18 +4,21 @@
             Home Page
         </h1>
         <HelloComponent/>
+        <ImageContainer/>
     </div>
 </template>
 
 <script>
 
 import HelloComponent from '@/component/HelloComponent.vue';
+import ImageContainer from '@/component/ImageContainer.vue';
 
 
 export default {
     name: 'HomePage',
     components: {
-        HelloComponent
+        HelloComponent,
+        ImageContainer
     }
 }
 
