09c4c1179a13efcfc6f106ac19b8e2afb65333dd
antoine <antoine.weisse@epitech.eu>
2024-12-05 22:00:17 +0100

    [add] bg

--- front/src/App.vue
+++ front/src/App.vue
@@ -26,8 +26,11 @@ html, body, #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: aqua;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
 }
 </style>
